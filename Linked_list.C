b5f81d2105e27e28aeda9684e64b9b79a7f9a67c
Nitesh Kumar <dev.exehub@gmail.com>
2023-11-08 22:14:11 +0530

    rename

--- Linked_list.C
+++ Linked_list.C
@@ -2,27 +2,31 @@
 #include <stdlib.h>
 
 // Define a structure for a node in the linked list
-struct Node {
+struct Node
+{
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
 // Function to insert a node at the end of the linked list
-void insertNode(struct Node** head, int value) {
+void insertNode(struct Node **head, int value)
+{
     // Create a new node
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->data = value;
     newNode->next = NULL;
 
     // If the list is empty, make the new node the head
-    if (*head == NULL) {
+    if (*head == NULL)
+    {
         *head = newNode;
         return;
     }
 
     // Otherwise, traverse the list to the last node
-    struct Node* current = *head;
-    while (current->next != NULL) {
+    struct Node *current = *head;
+    while (current->next != NULL)
+    {
         current = current->next;
     }
 
@@ -31,25 +35,29 @@ void insertNode(struct Node** head, int value) {
 }
 
 // Function to delete a node with a specific value from the linked list
-void deleteNode(struct Node** head, int value) {
-    struct Node* temp = *head;
-    struct Node* prev = NULL;
+void deleteNode(struct Node **head, int value)
+{
+    struct Node *temp = *head;
+    struct Node *prev = NULL;
 
     // If the head node itself holds the value to be deleted
-    if (temp != NULL && temp->data == value) {
+    if (temp != NULL && temp->data == value)
+    {
         *head = temp->next;
         free(temp);
         return;
     }
 
     // Search for the node to be deleted, keeping track of the previous node
-    while (temp != NULL && temp->data != value) {
+    while (temp != NULL && temp->data != value)
+    {
         prev = temp;
         temp = temp->next;
     }
 
     // If the value was not found in the list
-    if (temp == NULL) {
+    if (temp == NULL)
+    {
         printf("Node with value %d not found in the list.\n", value);
         return;
     }
@@ -60,17 +68,20 @@ void deleteNode(struct Node** head, int value) {
 }
 
 // Function to display the linked list
-void displayList(struct Node* head) {
-    struct Node* current = head;
-    while (current != NULL) {
+void displayList(struct Node *head)
+{
+    struct Node *current = head;
+    while (current != NULL)
+    {
         printf("%d -> ", current->data);
         current = current->next;
     }
     printf("NULL\n");
 }
 
-int main() {
-    struct Node* head = NULL;
+int main()
+{
+    struct Node *head = NULL;
 
     insertNode(&head, 1);
     insertNode(&head, 2);
